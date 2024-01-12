b457e31b5d059266f8e81e9da29e760180bed592
saraiesco <saraiescobar@google.com>
2024-01-12 16:15:26 -0600

    missing collection on model .... >:(

--- src/main/java/com/example/snippet/Snippet.java
+++ src/main/java/com/example/snippet/Snippet.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.*;
 @Data
+@Document(collection = "Snippets")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Snippet {
